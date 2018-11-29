dddb7d6aa41d558fd258c6262febdde8b5ebe5bf
yousuke_shimoji <shimoji.0701@outlook.jp>
2018-11-29 20:04:46 +0900

    3Dゲームアプリを作ろうvol.1　課題提出
    得点を表示するようにした。
    マルチタッチに対応できるようしした＞

---
+++ Assets/ScoreScript.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreScript : MonoBehaviour {
+    
+    private GameObject scoreText;
+    int a = 0;            
+    int b = 0;
+    int c = 0;
+    int d = 0;
+	// Use this for initialization
+	void Start () {
+        this.scoreText = GameObject.Find("ScoreText");
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        this.scoreText.GetComponent<Text>().text = "Score"+(a+b+c+d);
+             
+	}
+    private void OnCollisionEnter(Collision other){
+       
+       if(other.gameObject.tag =="SmallStarTag")     {　　　　　//SmallStarにあたると１加算
+            this.a += 1;　
+        }else if(other.gameObject.tag=="LargeStarTag" ) {　　　 //LargeStarにあたると２０加算　
+            this.b += 20;
+        }else if(other.gameObject.tag == "SmallCloudTag") {　　 //SmallCloudにあたると４０加算
+            this.c += 40;
+        }else if(other.gameObject.tag == "LargeCloudTag") {　　 //LargeCloudにあたると１００加算
+            this.d += 100;
+        }else {
+        }
+       
+
+        }
+    
+}
---
+++ Assets/TouchScript.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TouchScript : MonoBehaviour{
+    //HingeJointコンポーネントを入れる
+    private HingeJoint myHingeJoint;
+    //初期の傾き
+    private float defaultAngle = 20;
+    //弾いた時の傾き
+    private float flickAngle = -20;
+    int leftFrigerId = 0;
+    int rightFrigerId = 0;
+    // Use this for initialization
+    void Start()
+    {
+        this.myHingeJoint = GetComponent<HingeJoint>();
+        //フリッパーの傾きを設定
+        SetAngle(this.defaultAngle);
+
+        Debug.Log("Screen Width : " + Screen.width);
+    }
+
+    // Update is called once per frame
+    void Update()   {
+        Debug.LogFormat("タッチ数:{0}", Input.touchCount);
+        for (int i = 0; i < Input.touchCount; i++)
+        {
+            var id = Input.touches[i].fingerId;
+            var pos = Input.touches[i].position;
+            Debug.LogFormat("{0} - x:{1}, y:{2}", id, pos.x, pos.y);
+
+            if (Input.touches[i].phase == TouchPhase.Began){
+
+                if (pos.x < Screen.width / 2 && tag == "LeftFripperTag"){
+                    leftFrigerId = id;
+                    SetAngle(flickAngle);
+                }
+                if (pos.x > Screen.width / 2 && tag == "RightFripperTag"){
+                    rightFrigerId = id;
+                    SetAngle(flickAngle);
+                }
+            }else if(Input.touches[i].phase == TouchPhase.Ended) {
+                if( id == leftFrigerId && tag == "LeftFripperTag") {
+                    SetAngle(defaultAngle);
+                }else if( id == rightFrigerId && tag == "RightFripperTag") {
+                    SetAngle(defaultAngle);
+                }
+            }
+        }
+    }
+    public void SetAngle(float angle)
+    {
+        //フリッパーの傾きを設定
+        JointSpring jointSpr = this.myHingeJoint.spring;
+        jointSpr.targetPosition = angle;
+        this.myHingeJoint.spring = jointSpr;
+    }
+}
